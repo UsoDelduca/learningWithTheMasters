1340702d91933e646ded20cddea1cd5b40d366dd
UsoDelduca <usodelduca@gmail.com>
2021-09-02 10:32:09 +1200

    New exercises 02SEP21

---
+++ Exercicios dos mestres/Hide button/script.js
@@ -0,0 +1,7 @@
+function clique(){
+
+	let hideText = this.document.getElementById('first');
+	console.log('The button has been activated');
+	hideText.style.display = "none";
+	document.getElementById('toggle').innerText = "Well done";
+}
