f23346d4ef985e004500a24c7dff66b6d8cdf323
UsoDelduca <usodelduca@gmail.com>
2021-09-03 10:16:28 +1200

    dropdown button update

---
+++ Exercicios dos mestres/Dropdown button/script/script.js
@@ -0,0 +1,8 @@
+// button functions
+
+document.getElementById('firstBtn').onclick = function() {myFunction()};
+
+function myFunction() {
+	document.getElementById('BtnDropdown').classList.toggle("show");
+
+}
